7b2c051096f1a5632a93aa61b08e1ae407cdc7b0
lfox91 <lsfox91@gmail.com>
2015-12-23 14:33:05 -0800

    oppsies

--- client/assets/login.js
+++ client/assets/login.js
@@ -1,6 +1,6 @@
  $(document).ready( function(){
 	$(".button-primary").on('click', function(){
-		$.post("server/login.js", {uid: $(".id").val(), ut: $(".token").val} function(){
+		$.post("server/login.js", {uid: $(".id").val(), ut: $(".token").val()}, function(){
 			console.log("success");
 		});
 	});

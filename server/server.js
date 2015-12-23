7b1eeab4fd85db73a9e24fb54297a94d3781d37e
Geoffrey Cruz Welborn <gcruzwelborn@gmail.com>
2015-12-23 10:41:33 -0800

    added more skeleton files

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
 const http = require('http');
+
+var server = http.createServer(function(request, response) {
+  if (request.method === 'GET') {
+    if (request.url === '/') {
+      response.setHeader('Content-Type', 'text/html');
+      return response.end(fs.readFileSync(__dirname + '/client/index.html'));
+    }
+    if (request.url.match(/.js$|.html$|.css$|.woff|.woff2|.tff$/)) {
+      return response.end(fs.readFileSync(__dirname + "/client/assets/" + request.url));
+    }
+  }
+}).listen(3000)

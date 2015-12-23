09bc272b3112589c09728907cf169ff7055a1289
lfox91 <lsfox91@gmail.com>
2015-12-23 13:46:50 -0800

    commiting for merge

---
+++ client/login.js
@@ -0,0 +1,7 @@
+ $(document).ready( function(){
+	$(.button-primary).on('click', function(){
+		$.post("server/login.js", {uid: $(".id").val(), ut: $(".token").val} function(){
+			console.log("success");
+		});
+	});
+});
